49c1e1d506971dfbd6ada319d640493609c1e7a1
Hmilzy <1107766252@qq.com>
2022-08-27 20:17:09 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author HuMiao
+ * @create 2022-08-27 19:41
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+    }
+}
